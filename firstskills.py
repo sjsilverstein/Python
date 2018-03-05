20833d56609c3088ac2cfa8b85e229a7c1959e96
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-03-05 14:42:22 -0800

    Finishing strings and lists skills

---
+++ firstskills.py
@@ -0,0 +1,28 @@
+# Stephen Silverstein
+# https://github.com/sjsilverstein
+
+words = "It's thanksgiving day. It's my birthday,too!"
+print words.find('day')
+newString = words.replace('day', 'mounth')
+print newString
+x = [2,54,-2,7,12,98]
+print min(x)
+print max(x)
+x = ["hello",2,54,-2,7,12,98,"world"]
+print x[0] + x[len(x)-1]
+newList = []
+newList.append(x[0])
+newList.append(x[len(x)-1])
+print newList
+x = [19,2,54,-2,7,12,98,32,10,-3,6]
+x.sort()
+first = []
+last = []
+print x
+for idx in range(0, len(x)):
+	if idx <= (len(x)/2):
+		first.append(x[idx])
+	elif idx > (len(x)/2):
+		last.append(x[idx])
+print first
+print last

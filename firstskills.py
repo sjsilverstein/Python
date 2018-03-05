b6bd9b10bb6ef064baffe56cdab8dcafffeb9ab1
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-03-05 14:55:43 -0800

    Multiples, Sum, Average first skills

--- firstskills.py
+++ firstskills.py
@@ -1,28 +1,35 @@
 # Stephen Silverstein
 # https://github.com/sjsilverstein
 
-words = "It's thanksgiving day. It's my birthday,too!"
-print words.find('day')
-newString = words.replace('day', 'mounth')
-print newString
-x = [2,54,-2,7,12,98]
-print min(x)
-print max(x)
-x = ["hello",2,54,-2,7,12,98,"world"]
-print x[0] + x[len(x)-1]
-newList = []
-newList.append(x[0])
-newList.append(x[len(x)-1])
-print newList
-x = [19,2,54,-2,7,12,98,32,10,-3,6]
-x.sort()
-first = []
-last = []
-print x
-for idx in range(0, len(x)):
-	if idx <= (len(x)/2):
-		first.append(x[idx])
-	elif idx > (len(x)/2):
-		last.append(x[idx])
-print first
-print last
+# words = "It's thanksgiving day. It's my birthday,too!"
+# print words.find('day')
+# newString = words.replace('day', 'mounth')
+# print newString
+# x = [2,54,-2,7,12,98]
+# print min(x)
+# print max(x)
+# x = ["hello",2,54,-2,7,12,98,"world"]
+# print x[0] + x[len(x)-1]
+# newList = []
+# newList.append(x[0])
+# newList.append(x[len(x)-1])
+# print newList
+# x = [19,2,54,-2,7,12,98,32,10,-3,6]
+# x.sort()
+# first = []
+# last = []
+# print x
+# for idx in range(0, len(x)):
+# 	if idx <= (len(x)/2):
+# 		first.append(x[idx])
+# 	elif idx > (len(x)/2):
+# 		last.append(x[idx])
+# print first
+# # print last
+for num in range(1, 1001):
+	print num
+for num in range(5,1000001, 5):
+	print num
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+print sum(a)/len(a)

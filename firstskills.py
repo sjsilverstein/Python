d329c7627dff6a4b62f165c273291e45ec488c34
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-03-05 18:05:18 -0800

    finished list typing, running into an issue with the example strings without spaces continue to not have white space between one string and the next.

--- firstskills.py
+++ firstskills.py
@@ -32,49 +32,81 @@
 # 	print num
 # a = [1, 2, 5, 10, 255, 3]
 # print sum(a)
-# print sum(a)/len(a)
-sI = 45
-mI = 100
-bI = 455
-eI = 0
-spI = -23
-sS = "Rubber baby buggy bumpers"
-mS = "Experience is simply the name we give our mistakes"
-bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
-eS = ""
-aL = [1,7,4,21]
-mL = [3,5,7,34,3,2,113,65,8,89]
-lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
-eL = []
-spL = ['name','address','phone number','social security number']
-def filterType(testMe):
-	if isinstance(testMe, int) == True:
-		if testMe >= 100:
-			print "That's a big number!"
-		if testMe <=100:
-			print "That's a small number."
-	if isinstance(testMe, str) == True:
-		if len(testMe) >= 50:
-			print "Long Sentence."
-		if len(testMe) <50:
-			print "Short Sentence."
-	if isinstance(testMe, list) == True:
-		if len(testMe) >10:
-			print "Big List"
-		else:
-			print "Short List"
+# # print sum(a)/len(a)
+# sI = 45
+# mI = 100
+# bI = 455
+# eI = 0
+# spI = -23
+# sS = "Rubber baby buggy bumpers"
+# mS = "Experience is simply the name we give our mistakes"
+# bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+# eS = ""
+# aL = [1,7,4,21]
+# mL = [3,5,7,34,3,2,113,65,8,89]
+# lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+# eL = []
+# spL = ['name','address','phone number','social security number']
+# def filterType(testMe):
+# 	if isinstance(testMe, int) == True:
+# 		if testMe >= 100:
+# 			print "That's a big number!"
+# 		if testMe <=100:
+# 			print "That's a small number."
+# 	if isinstance(testMe, str) == True:
+# 		if len(testMe) >= 50:
+# 			print "Long Sentence."
+# 		if len(testMe) <50:
+# 			print "Short Sentence."
+# 	if isinstance(testMe, list) == True:
+# 		if len(testMe) >10:
+# 			print "Big List"
+# 		else:
+# 			print "Short List"
 
-filterType(sI)
-filterType(mI)
-filterType(bI)
-filterType(eI)
-filterType(spI)
-filterType(sS)
-filterType(mS)
-filterType(bS)
-filterType(eS)
-filterType(aL)
-filterType(mL)
-filterType(lL)
-filterType(eL)
-filterType(spL)
+# filterType(sI)
+# filterType(mI)
+# filterType(bI)
+# filterType(eI)
+# filterType(spI)
+# filterType(sS)
+# filterType(mS)
+# filterType(bS)
+# filterType(eS)
+# filterType(aL)
+# filterType(mL)
+# filterType(lL)
+# filterType(eL)
+# filterType(spL)
+
+# Write a program that takes a list and prints a message for each element in the list, based on that element's data type.
+
+# Your program input will always be a list. For each item in the list, test its data type. If the item is a string, 
+# concatenate it onto a new string. If it is a number, add it to a running sum. At the end of your program print the string, 
+# the number and an analysis of what the list contains. If it contains only one type, print that type, otherwise, print 'mixed'.
+
+def arrType(arr):
+	typeInt = False
+	typeStr = False
+	intSum = 0
+	strTotal = ""
+	for idx in range(0, len(arr)):
+		if isinstance(arr[idx], int):
+			intSum+=arr[idx]
+			typeInt = True
+		if isinstance(arr[idx], str):
+			strTotal+=arr[idx]
+			typeStr = True
+	if typeInt == True and typeStr == True:
+		print "The list you entered is of mixed type"
+		print "String:"+strTotal
+		print "Sum:" + str(intSum)
+	elif typeInt == True:
+		print "The list you entered is of integer type"
+		print "Sum:" + str(intSum)
+	else:
+		print "The list you entered is of string type"
+		print "String:"+ strtotal
+l = ['magical unicorns',19,'hello',98.98,'world']
+arrType(l)
+
